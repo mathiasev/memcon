4456ffda11b4cc28880bc0288457f9e6f9001423
mathiasev <mathiasev@hotmail.com>
2017-08-15 12:50:26 +1000

    Add Ajax JS

--- custom.js
+++ custom.js
@@ -1,9 +1,13 @@
-$(function() {
 
-$( "#form" ).submit(function( event ) {
-  console.log($('input[type="url"]').val());
-  event.preventDefault();
+$("#go").click(function(){
+    $.post("https://google.com",
+    {
+        name: "Donald Duck",
+        city: "Duckburg"
+    },
+    function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
 });
 
-
-});
+console.log("Loaded");
