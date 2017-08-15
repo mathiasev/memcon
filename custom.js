8bc83ffe0eabe9d7adf5c181f97310113959010e
mathiasev <mathiasev@hotmail.com>
2017-08-15 22:23:28 +1000

    Update

--- custom.js
+++ custom.js
@@ -18,4 +18,13 @@ $("#domains").click(function(){
     });
 });
 
+jQuery("input[id$=\'-value\']").change(function () {
+var total = 0.0;
+jQuery("input[id$=\'-value\']").each(function (index, value) {
+var val = parseFloat(jQuery("#" + index +"-qty").text()) * jQuery(value).val();
+jQuery("#" + index + "-total").text(val);
+total += val;
+});
+jQuery("#tot-val").text(total);});
+
 console.log("Ready");
