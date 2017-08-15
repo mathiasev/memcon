67a1955dffd0140877e4e4d4478bb596f6db52af
mathiasev <mathiasev@hotmail.com>
2017-08-15 18:46:57 +1000

    Now time to debug SQL

--- go.php
+++ go.php
@@ -107,7 +107,7 @@ $sqlStr .= '(' . $domain . '), ';
 endforeach;
 
 print_r($sqlStr);
-$sql = "INSERT IGNORE INTO domains (domainURI) VALUES " . substr($sqlStr,0,-1);
+$sql = "INSERT IGNORE INTO domains (domainURI) VALUES " . substr($sqlStr,0,-1) . ';';
 echo $sql;
 
 if (!$result = $mysqli->query($sql)) {
