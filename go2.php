5dbd9b5ae498ea931645552b1d858569004afe57
mathiasev <mathiasev@hotmail.com>
2017-08-16 17:25:02 +1000

    Checking that this actually works

--- go2.php
+++ go2.php
@@ -3,4 +3,19 @@
 /* ---- Image URL submitted by user ---- */
 $req_image_uri = (isset($_POST['url'])) ? $_POST['url']; 
 
+/* ---- Load Database ---- */
+$mysqli = new mysqli('localhost', 'generaluser', 'generalpass', 'memcon');
+if ($mysqli->connect_errno) { 
+    echo 'Error: Failed to make a MySQL connection.';
+    exit;
+}
+
 /* ---- Check if Image is registered ---- */
+checkImageExists($req_image_uri);
+
+function checkImageExists($req_image_uri) {
+    /* 1. See if image is in DB */
+
+    $sql = 'SELECT id FROM images WHERE imageURI = "$req_image_uri"';
+    echo $sql;
+}
