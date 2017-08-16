5fbc9cfda5d464ac54ed869d31f7ec2676123baf
mathiasev <mathiasev@hotmail.com>
2017-08-16 19:33:38 +1000

    Trying include

--- go2.php
+++ go2.php
@@ -11,6 +11,18 @@ if ($mysqli->connect_errno) {
 }
 
 /* ---- Set up CURL ---- */
+function getCURL($req_body, $req_url) {
+        $ch = curl_init(); 
+        curl_setopt($ch, CURLOPT_URL, $req_url); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		curl_setopt($ch, CURLOPT_POST,1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $req_body);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($req_format_body)) );
+        $output = curl_exec($ch); 
+        curl_close($ch);
+    return $output;
+}
+
 
 
 /* ---- If image isn't registered ---- */
@@ -21,7 +33,11 @@ endif;
 
 /* ---- Get Image Details from Google ---- */
 function getImageDetails($req_image_uri) {
-    /* 
+    /* Set Google Details */
+    include('/usr/ubuntu/apikey.php');
+    $req_key ;
+$req_url = 'https://vision.googleapis.com/v1/images:annotate?key=' . $req_key;
+
 
 }
 
