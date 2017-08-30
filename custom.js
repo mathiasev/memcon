e3905d0f4e885c2a7fc40b8829b26335f36d51fb
mathiasev <mathiasev@hotmail.com>
2017-08-30 18:16:05 +1000

    Go fix

--- custom.js
+++ custom.js
@@ -5,7 +5,8 @@ $("#go").click(function(){
         url: $('#url').val(),
     },
     function(data, status){
-		$('#result').html(data);
+		var js = "<script>jQuery(\"input[id$=\'-value\']\").change(function () {var total = 0.0;jQuery(\"input[id$=\'-value\']\").each(function (index, value) {var val = parseFloat(jQuery(\"#\" + index +\"-qty\").text()) * jQuery(value).val();jQuery(\"#\" + index + \"-total\").text(val);total += val;});jQuery(\"#tot-val\").text(total);});</script>";
+		$('#result').html(data + js);
 		$.scrollTo('#download',800);
     });
 });
@@ -20,14 +21,7 @@ $("#go2").click(function(){
     });
 });
 
-jQuery("input[id$=\'-value\']").change(function () {
-var total = 0.0;
-jQuery("input[id$=\'-value\']").each(function (index, value) {
-var val = parseFloat(jQuery("#" + index +"-qty").text()) * jQuery(value).val();
-jQuery("#" + index + "-total").text(val);
-total += val;
-});
-jQuery("#tot-val").text(total);});
+
 
 console.log("Ready");
 
